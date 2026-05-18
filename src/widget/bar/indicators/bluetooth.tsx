a59ae90c20a927b8308336f36f3725013bbd117d
Davi Azevedo <daviaaze@gmail.com>
2026-05-19 04:44:00 +0700

    feat(bar): show bluetooth device battery percentage next to bluetooth icon

--- src/widget/bar/indicators/bluetooth.tsx
+++ src/widget/bar/indicators/bluetooth.tsx
@@ -1,11 +1,25 @@
 import Bluetooth from "gi://AstalBluetooth"
 import Gtk from "gi://Gtk?version=4.0"
 import { createBinding, createComputed } from "gnim"
+import { toArray } from "#/lib/gjsUtils"
 
 export default () => {
   const bluetooth = Bluetooth.get_default()
 
   const isConnected = createBinding(bluetooth, "is-connected")
+  const isPowered = createBinding(bluetooth, "isPowered")
+
+  const batteryLabel = createComputed([
+    createBinding(bluetooth, "is-connected"),
+  ], (_connected) => {
+    const devices = toArray<any>(bluetooth.devices)
+    const connected = devices.filter(d => d.connected)
+    for (const device of connected) {
+      const bat = device.battery_percentage
+      if (bat >= 0) return `${bat.toFixed(0)}%`
+    }
+    return ""
+  })
 
   const tooltip = createComputed([
     createBinding(bluetooth, "is-connected")
@@ -17,11 +31,20 @@ export default () => {
     return connected.length > 0 ? connected.join(", ") : "Bluetooth"
   })
 
-  return <Gtk.Image
-    iconName={isConnected.as(c => c
-      ? "bluetooth-active-symbolic"
-      : "bluetooth-disconnected-symbolic")}
-    visible={createBinding(bluetooth, "isPowered")}
-    tooltipMarkup={tooltip}
-    pixelSize={18} />
+  return (
+    <Gtk.Box spacing={2}>
+      <Gtk.Image
+        iconName={isConnected.as(c => c
+          ? "bluetooth-active-symbolic"
+          : "bluetooth-disconnected-symbolic")}
+        visible={isPowered}
+        tooltipMarkup={tooltip}
+        pixelSize={18} />
+      <Gtk.Label
+        cssClasses={["bluetooth-battery"]}
+        visible={batteryLabel.as(l => l !== "")}
+        label={batteryLabel}
+      />
+    </Gtk.Box>
+  )
 }
