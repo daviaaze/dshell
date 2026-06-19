96efa3d8bf82fdb04c9dbbb6176042b16746975b
Davi Azevedo <daviaaze@gmail.com>
2026-06-19 20:11:19 -0300

    fix(quicksettings): use clock and sun icons for auto-sleep options
    - Timed options (5/15/60 min): preferences-system-time-symbolic (clock)
    - Indefinitely: weather-clear-symbolic (sun)

--- src/widget/quicksettings/button-grid/caffeinated.tsx
+++ src/widget/quicksettings/button-grid/caffeinated.tsx
@@ -30,25 +30,25 @@ export default () => {
       <LinkedBox>
         <Gtk.Button onClicked={() => inhibit.setDuration(0)}>
           <Adw.ButtonContent
-            iconName="list-add-symbolic"
+            iconName="weather-clear-symbolic"
             label="Indefinitely"
           />
         </Gtk.Button>
         <Gtk.Button onClicked={() => inhibit.setDuration(5)}>
           <Adw.ButtonContent
-            iconName="appointment-soon-symbolic"
+            iconName="preferences-system-time-symbolic"
             label="5 minutes"
           />
         </Gtk.Button>
         <Gtk.Button onClicked={() => inhibit.setDuration(15)}>
           <Adw.ButtonContent
-            iconName="appointment-soon-symbolic"
+            iconName="preferences-system-time-symbolic"
             label="15 minutes"
           />
         </Gtk.Button>
         <Gtk.Button onClicked={() => inhibit.setDuration(60)}>
           <Adw.ButtonContent
-            iconName="appointment-soon-symbolic"
+            iconName="preferences-system-time-symbolic"
             label="1 hour"
           />
         </Gtk.Button>
