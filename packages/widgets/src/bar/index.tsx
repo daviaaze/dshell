16bb006398a8a487548da94f72a297f78257b4d4
Davi Azevedo <daviaaze@gmail.com>
2026-08-04 12:33:05 -0300

    fix(ui): remove card shadow and rounded corners from bar

--- packages/widgets/src/bar/index.tsx
+++ packages/widgets/src/bar/index.tsx
@@ -100,7 +100,8 @@ function BarWindow({
                 });
             }}
             visible
-            cssClasses={['card', 'background']}
+            cssClasses={['background']}
+            css={'box-shadow: none; border-radius: 0;'}
             marginTop={position.as((p) => (p === BOTTOM ? 0 : BAR_MARGIN))}
             marginLeft={position.as((p) => (p === RIGHT ? 0 : BAR_MARGIN))}
             marginBottom={position.as((p) => (p === TOP ? 0 : BAR_MARGIN))}
